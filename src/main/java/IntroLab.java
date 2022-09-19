3c47068756efa7dccb0f5c1e3bc7f9f42167dc2e
Max Chen <maxlolm1k2l3@gmail.com>
2022-09-18 22:49:11 -0400

    关注嘉然，顿顿解馋!

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -40,6 +40,10 @@ public class IntroLab {
      * As a reference, we've provided the sample method exampleStudent.
      * Make sure you document your method properly!
      */
+    public static String chendej2(){
+        return printLabMessage("Hello", "Max Chen", true,
+                "关注嘉然，顿顿解馋!");
+    }
 
     /**
      * An example method that calls on printLabMessage.
@@ -59,7 +63,7 @@ public class IntroLab {
          * As a reference, we've provided a sample call to exampleStudent
          * above. Afterwards: run this file to see the output!
          */
-
+        System.out.println(chendej2());
 
     }
 
